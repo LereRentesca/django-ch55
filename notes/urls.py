a039536223a3ee055f7f309e53311d610098d0fb
LereRentesca <esteban.renteria@uabc.edu.mx>
2025-07-09 14:33:18 -0700

    Class #6

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('list/', views.NoteListView.as_view(), name="note_list"),
+    path('create/', views.NoteCreateView.as_view(), name="note_create"),
+    path('details/<int:pk>/', views.NoteDetailView.as_view(), name="note_details"),
+    path('save_comment/', views.save_comment, name="save_comment"),   
 ]
--- notes/views.py
+++ notes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .models import Note, NoteComment
 from django.urls import reverse_lazy
+from .forms import NoteForm
 
 
 # Create your views here.
@@ -21,3 +22,47 @@ LoginView    = LogIn
 class NoteListView(ListView):
     model = Note
     template_name = "notes/list.html"
+
+class NoteCreateView(CreateView):
+    model = Note
+    form_class = NoteForm
+    template_name = "notes/create.html"
+    success_url = reverse_lazy('note_list')
+
+    def get_context_data(self, **kwargs):
+        #allow us to extend/modify the context pass to the template
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Create Note"
+        return context
+    
+
+class NoteDetailView(DetailView):
+    model = Note
+    template_name = "notes/details.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # load all the comments for each note
+        note = self.object
+        comments = NoteComment.objects.filter(note=note).prefetch_related("author")
+        context["comments"] = comments
+        return context
+    
+
+
+def save_comment(request):
+    note_id = request.POST.get('note_id')
+    text = request.POST.get('content')
+    user = request.user #logged in user
+
+    note = Note.objects.get(id=note_id)
+
+    comment = NoteComment.objects.create(
+        note=note,
+        content=text,
+        author = user
+    )
+
+    comment.save()
+
+    return redirect('note_details', pk=note_id)
